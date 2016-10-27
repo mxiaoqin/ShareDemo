799b6aca71738ec9e247cdf6158d504fdb6b2ca5
mxiaoqin <2232711090@qq.com>
2016-10-27 18:16:34 +0800

    首次提交

---
+++ src/com/example/sharedemo/MainActivity.java
@@ -0,0 +1,79 @@
+﻿package com.example.sharedemo;
+
+import java.util.HashMap;
+
+import cn.sharesdk.framework.Platform;
+import cn.sharesdk.framework.PlatformActionListener;
+import cn.sharesdk.framework.ShareSDK;
+import cn.sharesdk.onekeyshare.OnekeyShare;
+import cn.sharesdk.sina.weibo.SinaWeibo;
+import android.os.Bundle;
+import android.os.Handler.Callback;
+import android.os.Message;
+import android.app.Activity;
+import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+public class MainActivity extends Activity implements OnClickListener,Callback{
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		ShareSDK.initSDK(this);
+		findViewById(R.id.shareweibo).setOnClickListener(this);
+
+	}
+
+	@Override
+	public void onClick(View v) {
+		// TODO Auto-generated method stub
+		switch (v.getId()) {
+		case R.id.shareweibo:
+			OnekeyShare oks = new OnekeyShare();
+			// 分享时Notification的图标和文字
+			oks.setNotification(R.drawable.ic_launcher,
+					MainActivity.this.getString(R.string.app_name));
+			// text是分享文本，所有平台都需要这个字段
+			oks.setText("分享内容");
+			
+			oks.show(v.getContext());
+			break;
+		}
+	}
+
+	@Override
+	protected void onDestroy() {
+		ShareSDK.stopSDK(this);
+		super.onDestroy();
+
+	}
+
+	@Override
+	public boolean handleMessage(Message msg) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
